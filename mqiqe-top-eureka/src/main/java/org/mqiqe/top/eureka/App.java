862fecf24b03506b40eff0dc8e59909ca5164ac7
Administrator <Administrator@greenvm-w09585v>
2018-08-17 10:46:08 +0800

    ew

---
+++ mqiqe-top-eureka/src/main/java/org/mqiqe/top/eureka/App.java
@@ -0,0 +1,13 @@
+package org.mqiqe.top.eureka;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
